3cb037921d2782e7fa02c3ae0f0e0fecf40d7586
Francis Kangwa Chanda <145052035+ZedBioinformatician@users.noreply.github.com>
2026-08-19 00:51:12 +0200

    Add files via upload

---
+++ Linux & BASh Scripting/Project 2 Installing Bioinformatic.sh
@@ -0,0 +1,28 @@
+#Project 2: Installing Bioinformatics Software on the Terminal
+
+conda activate base
+
+conda create -n funtools python=3.12.1
+
+cond activate funtools
+
+sudo apt update
+sudo apt install figlet
+
+figlet Francis_Kangwa_Chanda
+
+conda install bwa
+
+conda install blast
+
+conda install samtools
+
+conda install bedtools
+
+conda install spades
+
+conda install bcftools
+
+conda install fastp
+
+conda install multiqc
